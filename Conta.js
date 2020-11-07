00d7bcbe6a7590b9be6fddbc30ff4e013a10f4ba
Marcos <marcozzymandias@gmail.com>
2020-11-07 20:57:16 -0300

    Herança

--- Conta.js
+++ Conta.js
@@ -1,27 +1,40 @@
-export class Conta{
-    constructor(saldoInicial, cliente, agencia) {
-        this._saldo = saldoInicial;
-        this._cliente = cliente;
-        this._agencia = agencia;
-      }
-    
-      sacar(valor) {
-        taxa = 1.1 * valor;
-        if (this._saldo >= valor) {
-          this._saldo -= valor;
-          return valor;
-        }
-      }
-    
-      depositar(valor) {
-        if (valor <= 0) {
-          return;
-        }
-        this._saldo += valor;
-      }
-    
-      transferir(valor, conta) {
-        const valorSacado = this.sacar(valor);
-        conta.depositar(valorSacado);
-      }
+export class Conta { //Herança
+  constructor(saldoInicial, cliente, agencia) {
+    this._saldo = saldoInicial;
+    this._cliente = cliente;
+    this._agencia = agencia;
+  }
+
+  set cliente(novoValor) {
+    if (novoValor instanceof Cliente) {
+      this._cliente = novoValor;
+    }
+  }
+
+  get cliente() {
+    return this._cliente;
+  }
+
+  get saldo() {
+    return this._saldo;
+  }
+
+  sacar(valor) {
+    let taxa = 1;
+
+    const valorSacado = taxa * valor;
+    if (this._saldo >= valorSacado) {
+      this._saldo -= valorSacado;
+      return valorSacado;
+    }
+  }
+
+  depositar(valor) {
+    this._saldo += valor;
+  }
+
+  transferir(valor, conta) {
+    const valorSacado = this.sacar(valor);
+    conta.depositar(valorSacado);
+  }
 }
--- index.js
+++ index.js
@@ -5,11 +5,11 @@ import { Conta } from "./Conta.js";
 
 const cliente1 = new Cliente("Ricardo", 11122233309);
 
-const contaCorrenteRicardo = new Conta(0, cliente1, 1001);
+const contaCorrenteRicardo = new ContaCorrente(cliente1, 1001);
 contaCorrenteRicardo.depositar(500);
 contaCorrenteRicardo.sacar(100);
 
-const contaPoupanca = new Conta(50, cliente1, 1001);
+const contaPoupanca = new ContaPoupanca(50, cliente1, 1001);
 
 console.log(contaPoupanca);
 console.log(contaCorrenteRicardo);
