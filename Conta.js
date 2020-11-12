5c2a574af18f1070e5c9c38c63050d7ad7a42edd
Marcos <marcozzymandias@gmail.com>
2020-11-12 19:21:16 -0300

    fim da aula 03. Classes abstratas

--- Conta.js
+++ Conta.js
@@ -1,6 +1,12 @@
+//Classe abstrata
 export class Conta {
-  //Herança
   constructor(saldoInicial, cliente, agencia) {
+    if (this.constructor == Conta) {
+      throw new Error(
+        "Você não deveria instanciar um objeto do tipo Conta diretamente, pois essa é uma classe abstrata."
+      );
+    }
+
     this._saldo = saldoInicial;
     this._cliente = cliente;
     this._agencia = agencia;
@@ -20,9 +26,9 @@ export class Conta {
     return this._saldo;
   }
 
+  //Método abstrato
   sacar(valor) {
-    let taxa = 1;
-    return this._sacar(valor, taxa);
+   throw new Error ("O método Sacar da conta é abstrato")
   }
 
   _sacar(valor, taxa) {
