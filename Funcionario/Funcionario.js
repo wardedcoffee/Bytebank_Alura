59dfd5bf98fa3d290585ff257a097afe1e2de793
Marcos <marcozzymandias@gmail.com>
2020-11-12 21:19:04 -0300

    fim da aula 04. Sistema interno

--- Funcionario/Funcionario.js
+++ Funcionario/Funcionario.js
@@ -5,6 +5,14 @@ export class Funcionario{
         this._cpf = cpf;
 
         this._bonificacao = 1;
-        
+        this._senha;
+    }
+    
+    autenticar(senha){
+        return senha == this._senha;
+    }
+    
+    cadastrarSenha(senha){
+        this._senha = senha;
     }
 }
---
+++ SistemaAutenticacao.js
@@ -0,0 +1,9 @@
+/*  
+Ser autenticavel significa ter um método autenticar
+*/
+
+export class SistemaAutenticacao{
+    static login(autenticavel, senha){
+        return autenticavel.autenticar(senha);
+    }
+}
